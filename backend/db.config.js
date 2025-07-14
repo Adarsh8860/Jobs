6fa54759c061c64e53f832b47cea16f5fec802bc
Adarsh8860 <adarshkumar19172@gmail.com>
2025-07-14 12:47:34 +0530

    Updated API routes / bug fix / new feature

--- backend/db.config.js
+++ backend/db.config.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
+require('dotenv').config();
 exports.ConnectDB = async()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/mern')
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to MongoDB")
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`)
